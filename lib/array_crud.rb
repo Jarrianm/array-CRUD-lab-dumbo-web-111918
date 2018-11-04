4c1afe890cda1b6ab8c7dc3f7af1afd4b5a5a300
Jarrian McLean <jarrian.mclean@gmail.com>
2018-11-04 00:40:50 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-friends = ["kris","mike","jeff","ben","alex"]
+Array.new = ["kris","mike","jeff","ben","alex"]
 end
 
 def add_element_to_end_of_array(array, element)
